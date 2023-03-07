f19066ef8498060b466306239e87c7254128cc2e
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2023-03-07 05:51:05 +0100

    Basic framework

---
+++ README.md
@@ -0,0 +1,4 @@
+# TUI File Manager
+
+A simple and fast file manager for your terminal with a nice GUI
+
---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// FTXUI
+#include "ftxui/component/component.hpp"       // for Menu, Horizontal, Renderer
+
+using namespace ftxui;
+
+int main()
+{
+    std::cout << "Hello" << std::endl;
+    
+    return 0;
+}
